8d39767d2ec2b72109e7cea83585858798623fe8
Dave Snider <dave.snider@gmail.com>
2013-10-28 10:27:41 -0700

    setting up frontend env for rtd2. leave dependcies in gitignore for now.

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+
+  // load all grunt tasks
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  grunt.initConfig({
+    
+    compass: {
+      dev: {
+        options: {
+          config: 'media/sass/config.rb',
+          basePath: 'media/sass',
+          // outputStyle: 'compressed',
+          force: true
+        }
+      }
+    },
+    
+    exec: {
+      update: {
+        cmd: 'bower update'
+      },
+      runserver: {
+        cmd: 'cd readthedocs && ./manage.py runserver'
+      }
+    },
+    
+    watch: {
+      sass: {
+        files: ['media/**/*.sass', 'media/bower_compoents/**/*.sass'],
+        tasks: ['compass:dev']
+      },
+      /* watch our files for change, reload */
+      livereload: {
+        files: ['readthedocs/templates/**/*.html', 'media/css/*.css', '**/*.js'],
+        options: {
+          livereload: true
+        }
+      },
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-exec');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+
+  grunt.registerTask('default', ['exec:update', 'watch']);
+  grunt.registerTask('serve', ['exec:runserver']);
+
+}
+
+
+
