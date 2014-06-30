b24a0008b84961988b074512a9ec2e88443d1547
Anthony Johnson <aj@ohess.org>
2014-06-29 17:14:02 -0700

    Adding py3 support to hacks and hack tests
    Module import wasn't working correctly on py3 and further, py3.4 deprecates the
    imp module used to find and import modules. Fixing the logic here some so that
    various forms of imports work on py2/3

--- readthedocs/core/hacks.py
+++ readthedocs/core/hacks.py
@@ -1,14 +1,60 @@
-import imp
 import sys
+
+from django.utils import six
+
+# Dynamic find_module based on Python version
+if six.PY2:
+    import imp
+
+    def _find_module(name, path):
+        return imp.find_module(name, path)
+elif six.PY3:
+    # These calls are missing from importlib 1.x and Python 2
+    import importlib
+
+    # Cache names here, find_spec also iterates through `sys.meta_path` and will
+    # recurse endlessly.
+    _cache = []
+
+    def _find_module(name, path):
+        if name in _cache:
+            raise ImportError
+        _cache.append(name)
+        spec = importlib.util.find_spec(name, path)
+        if spec is not None:
+            return spec.loader
 
 
 class ErrorlessImport(object):
-    def find_module(self, name, path):
+    '''Mocked importer, returns mocked modules
+
+    This should be patched as the last finder in the :py:attr:`sys.meta_path`
+    chain. When using :py:func:`imp.find_module`, the first import call will
+    include the relative path -- i.e. for a module `foo.bar` importing
+    `itertools`, the first lookup is for `foo.bar.itertools`. Because of this,
+    we won't mock out anything in our namespace.
+
+    If the module name is not in our namespace, and isn't successfully found
+    using either :py:func:`imp.find_module` or :py:func:`importlib.find_spec`,
+    on Python 2 and 3 respectively, we will mock the module out.
+    '''
+
+    def find_module(self, name, path=None):
+        '''Find module, refer to self if we should mock the module'''
         try:
-            return imp.find_module(name, path)
+            module = _find_module(name, path)
+            return module
         except ImportError:
-            #raise
-            return FreeLoader()
+            if name.startswith('readthedocs'):
+                return None
+            else:
+                return self
+        return None
+
+    def load_module(self, fullname):
+        if fullname not in sys.modules:
+            sys.modules[fullname] = Mock()
+        return sys.modules[fullname]
 
 
 class Mock(object):
@@ -25,16 +71,11 @@ class Mock(object):
         return Mock()
 
 
-class FreeLoader:
-    def load_module(self, fullname):
-        return Mock()
-
-
 def patch_meta_path():
-    FreeLoader._class = ErrorlessImport()
-    sys.meta_path += [FreeLoader._class]
+    sys.meta_path.append(ErrorlessImport())
 
 
 def unpatch_meta_path():
-    sys.meta_path.remove(FreeLoader._class)
-    #sys.meta_path = []
+    for loader in sys.meta_path:
+        if isinstance(loader, ErrorlessImport):
+            sys.meta_path.remove(loader)
--- readthedocs/rtd_tests/tests/test_hacks.py
+++ readthedocs/rtd_tests/tests/test_hacks.py
@@ -1,4 +1,7 @@
+import sys
+
 from django.test import TestCase
+
 from core import hacks
 
 
@@ -11,10 +14,32 @@ class TestHacks(TestCase):
     def tearDown(self):
         hacks.unpatch_meta_path()
 
+    def test_hack_importer_order(self):
+        self.assertTrue(len(sys.meta_path) > 1)
+        self.assertTrue(isinstance(sys.meta_path[-1], hacks.ErrorlessImport))
+
     def test_hack_failed_import(self):
         import boogy
-        self.assertTrue(str(boogy), "<Silly Human, I'm not real>")
+        self.assertEqual(str(boogy), "<Silly Human, I'm not real>")
 
-    #def test_hack_correct_import(self):
-        #import itertools
-        #self.assertFalse(str(itertools), "<Silly Human, I'm not real>")
+    def test_hack_failed_local_import(self):
+        def _import():
+            import readthedocs.core.__non_existant__
+        self.assertRaises(ImportError, _import)
+
+    def test_hack_correct_import(self):
+        import itertools
+        self.assertNotEqual(str(itertools), "<Silly Human, I'm not real>")
+
+    def test_hack_correct_local_import(self):
+        import readthedocs.core.utils
+        self.assertNotEqual(str(readthedocs.core.utils),
+                            "<Silly Human, I'm not real>")
+
+    def test_hack_correct_relative_import(self):
+        import core.utils
+        self.assertNotEqual(str(core.utils), "<Silly Human, I'm not real>")
+
+    def test_hack_correct_relative_import_submodule(self):
+        from core import utils
+        self.assertNotEqual(str(utils), "<Silly Human, I'm not real>")
