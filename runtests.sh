be31714a721b0ab68fe30684fe9ed9557cdc7265
Anthony Johnson <aj@ohess.org>
2014-06-29 17:13:40 -0700

    Extending runtests helper

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,10 @@
 cd readthedocs
-DJANGO_SETTINGS_MODULE=settings.test ./manage.py test --logging-clear-handlers ./
+if [ $# -eq 0 ]; then
+    path=./
+else
+    path=$*
+fi
+DJANGO_SETTINGS_MODULE=settings.test ./manage.py test --logging-clear-handlers $path
 exit=$?
 cd -
 exit $exit
